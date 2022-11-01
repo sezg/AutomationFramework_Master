630fc485c85991bbe5ea93e3a2198228ee819bdb
sez <git@github.com:sezg/amazone_app.git>
2022-11-01 14:11:06 -0400

    New test data feature added.

--- src/main/java/com/envision/automation/framework/core/BaseTest.java
+++ src/main/java/com/envision/automation/framework/core/BaseTest.java
@@ -3,6 +3,8 @@ package com.envision.automation.framework.core;
 import com.envision.automation.framework.utils.ConfigLoader;
 import com.envision.automation.framework.utils.ExtentManager;
 import com.envision.automation.framework.utils.ExtentTestManager;
+import com.envision.automation.framework.utils.JsonUtils;
+import org.json.simple.parser.ParseException;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
@@ -14,11 +16,13 @@ import java.lang.reflect.Method;
 
 public class BaseTest {
     public WebDriver driver;
-
+    public JsonUtils jsonUtils;
     @BeforeSuite
-    public void loadConfigurations() throws IOException {
+    public void loadConfigurations() throws IOException, ParseException {
         ExtentManager.getReporter();
         ConfigLoader.loadConfigurations();
+        jsonUtils = new JsonUtils();
+        jsonUtils.loadTestDataFile("testData.json");
     }
 
 
--- src/test/java/com/envision/automation/tests/LoginTests.java
+++ src/test/java/com/envision/automation/tests/LoginTests.java
@@ -1,9 +1,11 @@
 package com.envision.automation.tests;
 
 import com.envision.automation.framework.core.BaseTest;
+import com.envision.automation.framework.utils.DataGenerator;
 import com.envision.automation.pages.HomePage;
 import com.envision.automation.pages.LandingPage;
 import com.envision.automation.pages.LoginPage;
+import org.json.simple.JSONObject;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -12,18 +14,46 @@ public class LoginTests extends BaseTest {
 
     @Test
     public void validateSuccessfulLoginToApplication() throws IOException, InterruptedException {
+        JSONObject loginData = jsonUtils.getJsonObject(jsonUtils.mainJsonObj, "loginData");
+        String userName = jsonUtils.getJsonObjectValue(loginData, "username");
+        String passWord = jsonUtils.getJsonObjectValue(loginData, "password");
+        String loginName = jsonUtils.getJsonObjectValue(loginData, "loginName");
+
+
         LandingPage landingPage = new LandingPage(driver);
         LoginPage loginPage = landingPage
                 .launchAutomationPracticeApplication()
                 .clickOnSignIn();
 
         HomePage homePage = loginPage
-                .enterUsername("hey@abc.com")
-                .enterPassword("Testing@1234")
+                .enterUsername(userName)
+                .enterPassword(passWord)
                 .clickOnSignIn();
 
         homePage
                 .checkIfSignOutDisplayed()
-                .checkIfUsernameLoggedInIsValid("Severus Snape");
+                .checkIfUsernameLoggedInIsValid(loginName);
+    }
+
+    @Test
+    public void validateSuccessfulLoginUsingRandomData() throws IOException, InterruptedException {
+
+        System.out.println(DataGenerator.getUsername()
+                +"/ "+DataGenerator.getPassword()
+                +"/ "+DataGenerator.getLoginName());
+
+        LandingPage landingPage = new LandingPage(driver);
+        LoginPage loginPage = landingPage
+                .launchAutomationPracticeApplication()
+                .clickOnSignIn();
+
+        HomePage homePage = loginPage
+                .enterUsername(DataGenerator.getUsername())
+                .enterPassword(DataGenerator.getPassword())
+                .clickOnSignIn();
+
+        homePage
+                .checkIfSignOutDisplayed()
+                .checkIfUsernameLoggedInIsValid(DataGenerator.getLoginName());
     }
 }
